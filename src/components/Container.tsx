50d70492faf2ae8c42f84d8d27995f4a42604d76
ceugeniag <gonzalezceugenia@gmail.com>
2025-06-04 21:08:08 -0300

    primer commit

--- src/components/Container.tsx
+++
@@ -1,5 +0,0 @@
-const Container = ({ children }: { children: React.ReactNode }) => {
-  return <div className="max-w-2xl mx-auto mb-8">{children}</div>;
-};
-
-export default Container;
--- src/components/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-const Navbar = () => {
-  return (
-    <nav className="bg-gray-900 text-gray-100 p-4 shadow-md shadow-black/60 mb-8">
-      <h1 className="text-4xl font-bold text-center text-white my-8">🎵 Mi Música</h1>
-    </nav>
-  );
-};
-
-export default Navbar;
